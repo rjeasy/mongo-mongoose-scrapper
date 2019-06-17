c84757fd4211432d3ecebb09802a8bfa0b6d235f
bigez <rjones26@outlook.com>
2019-06-16 20:46:32 -0700

    updates to create new DB

--- server.js
+++ server.js
@@ -26,8 +26,11 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
 
 // Routes
 
